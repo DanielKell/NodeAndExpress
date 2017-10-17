e2abd1cc71eec53e9e3640cb6bb99481158eedd6
DanielKell <danielkell.ubc@gmail.com>
2017-10-16 19:12:30 -0700

    Added route to request books by id

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// var MongoClient = require('mongodb').MongoClient;
+
+// var uri = "mongodb://dankell:<password>@cluster0-shard-00-00-ggsxz.mongodb.net:27017,cluster0-shard-00-01-ggsxz.mongodb.net:27017,cluster0-shard-00-02-ggsxz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+// MongoClient.connect(uri, function(err, db) {
+//   db.close();
+// });
+
 var express = require('express'),
     mongoose = require ('mongoose');
 
