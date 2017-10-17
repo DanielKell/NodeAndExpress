775f1a20a4d1b9f9aefcdd00979da40f0d0aa1b7
DanielKell <danielkell.ubc@gmail.com>
2017-10-16 18:26:12 -0700

    Added simple routing to /api/books

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ var app = express();
 var port = process.env.PORT || 3000;
 //Look for a port and if it doesnt exist, give it 3000
 
+//Creating a router
+let bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(request, response) {
+        var responseJson = {hello: "this is my api"};
+
+        response.json(responseJson);
+    });
+
+app.use('/api', bookRouter); //Load all of the API routes 
+
 //Setting up a handler. Use a / and a callback function
 app.get('/', function(request, response){
     response.send('welcome');
